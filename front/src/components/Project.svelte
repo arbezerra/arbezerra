ae43abc7ed9894ea8447638e9281a51f05aa2896
arbezerra <arbezerra@gmail.com>
2023-07-12 18:03:29 -0300

    Project skeleton

---
+++ front/src/components/Project.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { IProject } from "../interfaces/Project";
+
+	export let project: IProject;
+</script>
+
+<h2>{project.title}</h2>
+<p>{project.description}</p>
--- front/src/components/Projects.svelte
+++ front/src/components/Projects.svelte
@@ -1,3 +1,13 @@
-<section class="">
-	<h1>Projects</h1>
+<script lang="ts">
+	import type { IProject } from "../interfaces/Project";
+	import Project from "./Project.svelte";
+
+	const projects: IProject[] = [{ id: '1', title: 'Title', description: "Description" }];
+</script>
+
+<section class="py-8">
+	<h1 class="text-4xl font-bold mb-8">Projects</h1>
+	{#each projects as project}
+		<Project {project} />
+	{/each}
 </section>
