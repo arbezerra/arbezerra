e260663ff06368041ac0c4a90ca9cf540010ee4d
arbezerra <arbezerra@gmail.com>
2023-07-21 12:56:45 -0300

    feat: Category

--- front/src/components/Posts.svelte
+++ front/src/components/Posts.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+	import type { ICategory } from '../interfaces/Category';
 	import type { IPost } from '../interfaces/Post';
+	import Badge from './Badge.svelte';
 	import Post from './Post.svelte';
 
+	export let category: ICategory | undefined = undefined;
 	export let posts: IPost[];
 </script>
 
 <section class="py-8">
 	<a href="/posts">
-		<h1 class="text-4xl font-bold mb-8">Posts</h1>
+		<h1 class="text-4xl font-bold mb-8">
+			{#if category}
+				Category: {category.name}
+			{:else}
+				Posts
+			{/if}
+		</h1>
 	</a>
 	<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
 		{#each posts as post}
---
+++ front/src/routes/category/[slug]/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Posts from '../../../components/Posts.svelte';
+	import type { ICategory } from '../../../interfaces/Category';
+	import type { IPost } from '../../../interfaces/Post';
+	const category: ICategory = {
+		id: "1",
+		slug: "test",
+		name: "Test"
+	}
+	const posts: IPost[] = [
+		{
+			id: '1',
+			slug: 'hello-world',
+			title: 'First Post',
+			content: 'Hello world!',
+			summary: 'Summary',
+			date: new Date(),
+			category: {
+				id: '1',
+				name: 'Teste',
+				slug: 'test'
+			}
+		},
+		{
+			id: '2',
+			slug: 'hello-world2',
+			title: 'Second Post',
+			content: 'Hello world 2!',
+			summary: 'Summary 2',
+			date: new Date(),
+			category: {
+				id: '1',
+				name: 'Teste',
+				slug: 'test'
+			}
+		}
+	];
+</script>
+
+<Posts {posts} {category} />
