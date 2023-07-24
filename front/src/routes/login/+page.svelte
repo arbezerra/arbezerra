d0baaa37e61a816e7a447fbe245d2aab7ed7471c
arbezerra <arbezerra@gmail.com>
2023-07-24 10:52:28 -0300

    feat: Login page

---
+++ front/src/routes/login/+page.svelte
@@ -0,0 +1,53 @@
+<div class="flex flex-col justify-center">
+	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+		<h2 class="mt-10 text-center text-4xl font-bold leading-9 tracking-tight">
+			Sign in
+		</h2>
+	</div>
+
+	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+		<form class="space-y-6" action="#" method="POST">
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6"
+					>Email address</label
+				>
+				<div class="mt-2">
+					<input
+						id="email"
+						name="email"
+						type="email"
+						autocomplete="email"
+						required
+						class="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-default-light text-default-dark placeholder:text-default-dark focus:ring-2 focus:ring-inset focus:ring-primary-light sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<div class="flex items-center justify-between">
+					<label for="password" class="block text-sm font-medium leading-6"
+						>Password</label
+					>
+				</div>
+				<div class="mt-2">
+					<input
+						id="password"
+						name="password"
+						type="password"
+						autocomplete="current-password"
+						required
+						class="block w-full rounded-md border-0 p-1.5 shadow-sm ring-1 ring-inset ring-default-light text-default-dark placeholder:text-default-dark focus:ring-2 focus:ring-inset focus:ring-primary-light sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<button
+					type="submit"
+					class="flex w-full justify-center rounded-md bg-primary-light px-3 p-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-light"
+					>Sign in</button
+				>
+			</div>
+		</form>
+	</div>
+</div>
