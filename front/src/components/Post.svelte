999b6c22f2dfa59332b9a74c742e5bf02adb7572
arbezerra <arbezerra@gmail.com>
2023-09-25 02:33:18 -0300

    fix: Post content margin and color

--- front/src/components/Post.svelte
+++ front/src/components/Post.svelte
@@ -11,10 +11,10 @@
 		<small class="text-xs mr-2">{post.date.toLocaleString()}</small>
 		<a href="/category/{post.category_slug}"><Badge text={post.category} /></a>
 	</div>
-	<a href="/post/{post.slug}">
+	<a href="/post/{post.slug}" class="mb-4">
 		<h1 class="{short ? 'text-2xl' : 'text-4xl'} font-bold mb-2">{post.title}</h1>
 	</a>
-    <div class="prose">
+    <div class="prose dark:prose-invert">
         {@html short ? post.summary : post.content}
     </div>
 </article>
