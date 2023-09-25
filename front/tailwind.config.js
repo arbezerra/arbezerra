bb14219d1e63f6436a973c049063fc98256ec028
arbezerra <arbezerra@gmail.com>
2023-09-25 02:03:12 -0300

    feat: Add tailwind prose

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,24 +2,26 @@ const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				default: {
-					light: colors.slate[100],
-					dark: colors.slate[900]
-				},
-				neutral: {
-					light: colors.slate[300],
-					dark: colors.slate[600]
-				},
-				primary: {
-					light: colors.indigo[600],
-					dark: colors.indigo[800]
-				}
-			}
-		}
-	},
-	plugins: []
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                default: {
+                    light: colors.slate[100],
+                    dark: colors.slate[900]
+                },
+                neutral: {
+                    light: colors.slate[300],
+                    dark: colors.slate[600]
+                },
+                primary: {
+                    light: colors.indigo[600],
+                    dark: colors.indigo[800]
+                }
+            }
+        }
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ]
 };
